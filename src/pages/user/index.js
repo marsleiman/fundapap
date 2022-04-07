0b69d84c2ed5a97c7b2dd473503f9251225a4fee
Marito Vera <35548529+Marit007@users.noreply.github.com>
2022-04-06 21:55:05 -0300

    add table, add meetings, add router (#1)
    Co-authored-by: “MarioVera” <“mario.vera@mercadolibre.com”>

--- src/pages/user/index.js
+++ src/pages/user/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import { useUser } from '../../hooks/use-user';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 export default function User() {
   const { user } = useUser();
@@ -8,9 +16,51 @@ export default function User() {
 
   const logeate = <h1>{`Hola, logueate maestro`}</h1>;
 
+  const useStyles = makeStyles({
+    table: {
+      minWidth: 650,
+    },
+  });
+
+  function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+  }
+
+  const rows = [
+    createData(new Date().toDateString()),
+    createData(new Date().toDateString()),
+  ];
+
+  const TableMeets = () => {
+    const classes = useStyles();
+
+    return (
+        <TableContainer component={Paper}>
+          <Table className={classes.table} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Reuniones Asistidas</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                  <TableRow key={row.name}>
+                    <TableCell component="th" scope="row">
+                      {row.name}
+                    </TableCell>
+                  </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+    );
+  }
+
+
   return (
     <div>
       {user.name ? component : logeate}
+      <TableMeets />
     </div>
     );
 }
