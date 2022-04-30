dadf4eb6161872ec76b6c0c5c53b4ee7441f401f
marsleiman <maaar.iam94@gmail.com>
2022-04-30 13:57:01 -0300

    fix styles table

--- src/pages/home/calendar/index.js
+++ src/pages/home/calendar/index.js
@@ -1,8 +1,4 @@
 import * as React from 'react';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import Divider from '@mui/material/Divider';
-import ListItemText from '@mui/material/ListItemText';
 
 function Calendar(props) {
   return (
